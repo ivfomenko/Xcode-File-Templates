09f30896a8a3859f79909fdbb2f7a2276392f237
Ivan Fomenko <ifomenko_ext@babbel.com>
2023-08-27 18:07:19 +0200

    introduce json and stencil templates
    - introduce json and stencil templates

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ viperDirectory="Templates/File Template/VIPER"
 testsDirectory="Templates/File Template/Tests"
 mvvmDirectory="Templates/File Template/MVVM"
 commonServicesDirectory="Templates/File Template/Common Services"
+toolsDirectory="Templates/File Template/Tools"
 
 mkdir -p "${installDirectory}"
 
@@ -12,5 +13,6 @@ cp -R "${viperDirectory}" "${installDirectory}/VIPER/"
 cp -R "${testsDirectory}" "${installDirectory}/Tests/"
 cp -R "${mvvmDirectory}" "${installDirectory}/MVVM/"
 cp -R "${commonServicesDirectory}" "${installDirectory}/Core/"
+cp -R "${toolsDirectory}" "${installDirectory}/Tools/"
 
 echo "✅ Templates installed to ${installDirectory}"
